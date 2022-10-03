7aa043e3cf6d987be971680d3eb2c87e0f7094f7
Michael <info@academic.gift>
2022-10-03 18:30:28 +0300

    Upload_to added

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django_countries.fields import CountryField
 
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -14,6 +19,7 @@ class Customer(models.Model):
     online = models.BooleanField(default=False)
     country = CountryField(blank=True, null=True)
     city = models.CharField(max_length=100, blank=True, null=True)
+    city = models.FileField(upload_to=user_directory_path)
 
     def __str__(self):
         return self.username
